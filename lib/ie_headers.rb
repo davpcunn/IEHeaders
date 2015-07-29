a978769a4a089718045346c1a256dfb963771103
davpcunn <david@preferral.com>
2015-07-29 20:02:17 +0000

    adds no compatability header to response header

--- lib/ie_headers.rb
+++ lib/ie_headers.rb
@@ -1,15 +1,18 @@
 require "ie_headers/version"
 
 module IEHeaders
+
   class IEHeaders
     def initialize(app)
       @app = app
     end
+
     def call(env)
       status, headers, response = @app.call(env)
-      #add header to disable Internet Explorer Compatability Mode
+      #add header that disable Internet Explorer Compatability Mode to the response
       headers['X-UA-Compatible'] = 'IE=edge'
       [status, headers, response]
     end
   end
+
 end
